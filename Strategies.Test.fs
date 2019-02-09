3f6ce6fe25663982712ea352121ab73e7fe9d43a
David Gruar <davidgruar@gmail.com>
2019-02-09 17:14:29 +0000

    Add tests

---
+++ Strategies.Test.fs
@@ -0,0 +1,40 @@
+module ShutTheBox.StrategiesTests
+open Xunit
+open FsUnit
+open ShutTheBox.Strategies
+
+let lowestFirstTestData: obj[][] = [|
+    [|[1..9]; 2; Some [2]|]
+    [|[1..9]; 3; Some [1; 2]|]
+    [|[1..9]; 4; Some [1; 3]|]
+    [|[1..9]; 9; Some [1; 8]|]
+    [|[1..9]; 10; Some [1; 9]|]
+    [|[1..9]; 12; Some [3; 9]|]
+    [|[7; 8; 9]; 6; None|]
+    [|[9]; 9; Some [9]|]
+    [|[1; 2; 3]; 6; None|] // Should really return [1; 2; 3]
+|]
+
+
+[<Theory>]
+[<MemberData("lowestFirstTestData")>]
+let lowestFirstTest (box, target, expected) =
+    let result = lowestFirst target box
+    result |> should equal expected
+
+let highestFirstTestData: obj[][] = [|
+    [|[1..9]; 2; Some [2]|]
+    [|[1..9]; 3; Some [3]|]
+    [|[1..9]; 9; Some [9]|]
+    [|[1..9]; 10; Some [9; 1]|]
+    [|[1..9]; 12; Some [9; 3]|]
+    [|[7; 8; 9]; 6; None|]
+    [|[9]; 9; Some [9]|]
+|]
+
+
+[<Theory>]
+[<MemberData("highestFirstTestData")>]
+let highestFirstTest (box, target, expected) =
+    let result = highestFirst target box
+    result |> should equal expected
